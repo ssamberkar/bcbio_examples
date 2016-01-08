2d7ba36e771bea3a110a4704b429f2620aac7c9e
Mike Croucher <michael.p.croucher@googlemail.com>
2016-01-08 17:05:36 +0000

    Switch to hg38 and attempt to run the analysis

--- submit_bcbiojob.sh
+++ submit_bcbiojob.sh
@@ -6,7 +6,7 @@
 #$ -P hidelab
 
 module load apps/gcc/5.2/bcbio/0.9.6a
-work_dir='/shared/hidelab2/user/md4zsa/Work/TDP_Omics_Study'
+work_dir='/fastdata/fe1mpc/sandeep/TDP_Omics_Study'
 
 #Seq.Reads file directories
 tdp43_r1_files=$work_dir/Data/R1
@@ -17,7 +17,18 @@ tdp43_r1=($(find $tdp43_r1_files -type f -name "*.gz"|sort -n))
 tdp43_r2=($(find $tdp43_r2_files -type f -name "*.gz"|sort -n))
 
 #Download the best-practice template file for RNAseq experiment
+echo "DOWNLOADING TEMPLATE"
 bcbio_nextgen.py -w template illumina-rnaseq tdp43_project
 
-#Initiate the main analysis
+#Edit the template
+echo "EDITTING TEMPLATE"
+sed -i 's/GRCh37/hg38/g' $work_dir/tdp43_project/config/tdp43_project-template.yaml
+
+#Initialise the main analysis
+echo "INITIALISING ANALYSIS"
 bcbio_nextgen.py -w template $work_dir/tdp43_project/config/tdp43_project-template.yaml $work_dir/tdp43_project ${tdp43_r1[@]} ${tdp43_r2[@]}
+
+#Perform the analysis
+echo "PERFOMING ANALYSIS"
+cd $work_dir/tdp43_project/work
+bcbio_nextgen.py ../config/tdp43_project.yaml
