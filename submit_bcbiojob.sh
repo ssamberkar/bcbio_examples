9fe3621176d6f58ae4d3874bd2e9380dbb055f04
Sandeep Amberkar <sandeep.amberkar18@gmail.com>
2016-01-14 13:23:40 +0000

    this is the current running script
    Changed to star over hisat2 due to index issue
    Changed to GRCh37 over hg38 as per recommended guidelines for STAR

--- submit_bcbiojob.sh
+++ submit_bcbiojob.sh
@@ -22,16 +22,21 @@ bcbio_nextgen.py -w template illumina-rnaseq tdp43_project
 
 #Edit the template
 echo "EDITTING TEMPLATE"
-#Switch to the hg38 genome
-sed -i 's/GRCh37/hg38/g' $work_dir/tdp43_project/config/tdp43_project-template.yaml
-#Switch to using hisat2
-sed -i 's/tophat2/hisat2/g' $work_dir/tdp43_project/config/tdp43_project-template.yaml
+#Switch to the GRCh37 over hg38 genome for STAR
+sed -i 's/hg38/GRCh37/g' $work_dir/tdp43_project/config/tdp43_project-template.yaml
+#Switch to using star over tophat2 for performance and hisat2 as hisat2 indices fail to load
+sed -i 's/tophat2/star/g' $work_dir/tdp43_project/config/tdp43_project-template.yaml
 
 #Initialise the main analysis
+# tdp43_project.csv is a metadata file. It should be of the form (more details - https://bcbio-nextgen.readthedocs.org/en/latest/);
+# samplename,description,phenotype,batch
+# sample_1,Test,V717L,1
+# sample_2,Control,Neg,1
 echo "INITIALISING ANALYSIS"
-bcbio_nextgen.py -w template $work_dir/tdp43_project/config/tdp43_project-template.yaml $work_dir/tdp43_project ${tdp43_r1[@]} ${tdp43_r2[@]}
+bcbio_nextgen.py -w template $work_dir/tdp43_project/config/tdp43_project-template.yaml $work_dir/tdp43_project.csv ${tdp43_r1[@]} ${tdp43_r2[@]}
 
 #Perform the analysis
 echo "PERFOMING ANALYSIS"
 cd $work_dir/tdp43_project/work
-bcbio_nextgen.py ../config/tdp43_project.yaml
+# -n parameter states the number or cores available for bcbio
+bcbio_nextgen.py -n 16 ../config/tdp43_project.yaml
